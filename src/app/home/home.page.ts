8debd381cfee300749d4a4da86ed849492d60181
Tanguy Jouvin <42809139+tanguyJouvin@users.noreply.github.com>
2021-01-18 17:08:50 +0100

    AJOUT - fonction cacher bouton du questionnaire

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  //déclaration des variables
+  pseudo: String = '';
+  message: String = '';
+  listeDifficulte: Array<string> = ['easy', 'medium', 'hard'];
+  difficulte: String = null;
+  connecte: Boolean = false;
+  clique: Boolean = false;
+
   constructor() {}
+
+  verifSaisie() : String
+  {
+
+    if(this.pseudo.length > 3 || this.difficulte != null)
+    {
+      this.connecte = true;
+      this.message = `Bienvenue sur OpenTrivia ${this.pseudo}`;
+    }else{
+      this.connecte;
+      this.message = "Vous n'avez pas saisie de bonnes informations";
+    }
+    return this.message;
+  }
+
+  controlSaisie() 
+  {
+    return !this.clique;
+    
+  }
+  deconnexion() : Boolean
+  {
+    return this.connecte;
+  }
+  
 
 }
